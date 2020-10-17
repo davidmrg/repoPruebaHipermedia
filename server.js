9430882498860e07fd303bec8151b989b4721fca
David M Ramirez <davidramirez@MacBook-Pro-de-David.local>
2020-10-17 12:21:28 -0500

    primer commit

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const router = express.Router();
 
 //importar las rutas:
 const indexRoutes = require('./routes/index');
- 
+
+ // acceso a vars de entorno global para poder recibir el puerto que el proveedor conceda.
+const port = process.env.PORT || 3000;
+
 // middleware:
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended: false})); //entiende los datos que vienen desde un form html
@@ -16,8 +19,8 @@ app.set('view engine','hbs');
 
 // cada vez que el server reciba peticion a /, usaremos indexRoutes
 app.use('/', indexRoutes);
-
-app.listen(3000, () => {
-    console.log('Servidor corriendo en http://localhost:3000');
+ 
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
 });
 
